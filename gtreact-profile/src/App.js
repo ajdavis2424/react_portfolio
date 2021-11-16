b18a8d48533859d539fe52fdbfdbdb49c190919f
ALexander Davis <ajdavis2424@gmail.com>
2021-11-16 17:37:44 -0500

    redefined components, and created rfce shells for each template

--- gtreact-profile/src/App.js
+++ gtreact-profile/src/App.js
@@ -1,17 +1,13 @@
 // Components are imported here
-import Header from './Components/Header';
-import Project from './Components/Project';
-import Footer from './Components/Footer';
+
 
 
 // Componets are passed through here in App.js
 function App() {
   return (
       <main className= "app">
-        <Header />
-        <About />
-        <Project />
-        <Footer />
+        hello
+        
       </main>
     );
 }
