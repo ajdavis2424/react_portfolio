9123a4fc1e1e2c2bc36888945b66944129053fcb
ALexander Davis <ajdavis2424@gmail.com>
2021-11-16 15:38:03 -0500

    file clean up, code clean up to start

--- gtreact-profile/src/App.js
+++ gtreact-profile/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// Components are imported here
+import Header from './Components/Header';
+import Project from './Components/Project';
+import Footer from './Components/Footer';
 
+
+// Componets are passed through here in App.js
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <main className= "app">
+        <Header />
+        <About />
+        <Project />
+        <Footer />
+      </main>
+    );
 }
 
 export default App;
