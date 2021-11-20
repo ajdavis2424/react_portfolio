16dd516902646e2abf943c2a3ae66bfa8ba7d4b7
ALexander Davis <ajdavis2424@gmail.com>
2021-11-19 22:10:19 -0500

    intro page, but missing main picture

--- gtreact-profile/src/Components/Intro/Intro.jsx
+++ gtreact-profile/src/Components/Intro/Intro.jsx
@@ -1,9 +1,24 @@
 import "./intro.scss"
-
+import { init } from 'ityped'
+ 
 function Intro() {
     return (
         <div className="intro" id="intro">
-            
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="gtreact-profile/public/assets/IMG_1380.jpeg" alt=""/>
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1> Alexander J. Davis</h1>
+                    <h3> Full-Stack <span>Engineer</span></h3>
+                </div>
+                <a href="#portfolio">
+                down arrow here
+                </a>
+            </div>
         </div>
     )
 }
