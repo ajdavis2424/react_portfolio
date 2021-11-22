2faa9c41c8805ee0fba355950489ceab671d1fee
ALexander Davis <ajdavis2424@gmail.com>
2021-11-21 23:25:05 -0500

    active classes finished for portfolio portion, adding  dynamic hooks

--- gtreact-profile/src/Components/Portfolio/Portfolio.jsx
+++ gtreact-profile/src/Components/Portfolio/Portfolio.jsx
@@ -1,9 +1,80 @@
-import "./portfolio.scss"
+import { useEffect, useState } from "react";
+import PortfolioList from "../PortfolioList/PortfolioList";
+import "./portfolio.scss";
+import {
+    featuredPortfolio, designPortfolio, contentPortfolio, } from "../../data"
 
 function Portfolio() {
+    // whenever something from array is selected it will be active class
+        const [selected, setSelected] = useState("featured");
+    const list =[
+        {
+            id: "featured",
+        title: "Featured",
+        },
+        {
+            id: "web",
+        title: "Web App",
+        },
+        {
+            id: "mobile",
+        title: "Mobile App",
+        },
+        {
+            id: "design",
+        title: "Design",
+        },
+        {
+            id: "content",
+        title: "Content",
+        },
+    ];
     return (
         <div className="portfolio" id="portfolio">
-            
+            <h1>Portfolio</h1>
+            <ul>
+                {/* map through array above listed as "list" and return PortfolioList */}
+                {list.map((item) => (
+                    <PortfolioList title={item.title} 
+                    active={selected === item.id} 
+                    setSelected={setSelected}
+                    id={item.id}
+                    />
+                ))}
+                
+                
+                {/* <li className="active">Featured</li>
+                <li>Web App</li>
+                <li>Mobile App</li>
+                <li>Designing</li>
+                <li>Branding</li> */}
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="gtreact-profile/public/assets/images/Weather App Image.png" alt="" />
+                    <h3>Weather App</h3>
+                </div>
+                <div className="item">
+                    <img src="gtreact-profile/public/assets/images/Weather App Image.png" alt="" />
+                    <h3>Weather App</h3>
+                </div>
+                <div className="item">
+                    <img src="gtreact-profile/public/assets/images/Weather App Image.png" alt="" />
+                    <h3>Weather App</h3>
+                </div>
+                <div className="item">
+                    <img src="gtreact-profile/public/assets/images/Weather App Image.png" alt="" />
+                    <h3>Weather App</h3>
+                </div>
+                <div className="item">
+                    <img src="gtreact-profile/public/assets/images/Weather App Image.png" alt="" />
+                    <h3>Weather App</h3>
+                </div>
+                <div className="item">
+                    <img src="gtreact-profile/public/assets/images/Weather App Image.png" alt="" />
+                    <h3>Weather App</h3>
+                </div>
+            </div>
         </div>
     )
 }
---
+++ gtreact-profile/src/data.js
@@ -0,0 +1,59 @@
+// HERE IS THE FEATURED PORTFOLIO
+export const featuredPortfolio = [
+    {
+        id: 1,
+    title: "GoodMorning App",
+    img:""
+    },
+    {
+        id: 2,
+    title: "Weather Dashboard App",
+    img:""
+    },
+    {
+        id: 3,
+    title: "Password Generator",
+    img:""
+    },
+    {
+        id: 4,
+    title: "TuneTime App",
+    img:""
+    },
+    {
+        id: 5,
+    title: "Ecommerce Backend",
+    img:""
+    },
+    {
+        id: 6,
+    title: "MongoDB Projects",
+    img:""
+    }
+];
+
+export const designPortfolio = [
+    {
+        id: 1,
+        title: "Traditional Portfolio Website",
+        img:""
+    },
+    {
+        id: 2,
+    title: "Code Refactoring Project",
+    img:""
+    }
+];
+
+export const contentPortfolio =[
+    {
+        id: 1,
+        title: "Demo for Password Generator",
+        img:""
+    },
+    {
+        id: 2,
+    title: "Demo for Ecommerce Backend",
+    img:""
+    }
+]
